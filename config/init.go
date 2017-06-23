cfee715b477b72f8eaba6138ee63e3cd115c777a
Lester <lesterzone@gmail.com>
2017-06-23 13:39:27 -0600

    Use flags to provide configuration values
    Allow user specify config via flags:
    	./blog-generator -repo git@github.com:username/repo.git

---
+++ config/init.go
@@ -0,0 +1,10 @@
+package config
+
+// RepoURL is the URL of the data-source repository
+var RepoURL string
+
+// TmpFolder is the folder where the data-source repo is checked out to
+var TmpFolder string
+
+// DestFolder is the output folder of the static blog
+var DestFolder string
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/zupzup/blog-generator/cli"
+	"github.com/zupzup/blog-generator/config"
 )
 
+func init() {
+	flag.StringVar(&config.RepoURL, "repo", "", "Repository URL")
+	flag.StringVar(&config.TmpFolder, "tmpfolder", "./tmp", "folder where the data-source repo is checked out to")
+	flag.StringVar(&config.DestFolder, "destfolder", "./www", "is the output folder of the static blog")
+}
+
 func main() {
+	// Get flags and set values to vars defined on init
+	flag.Parse()
 	cli.Run()
 }
