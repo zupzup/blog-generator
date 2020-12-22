f1a1ba040bceaaaebed8a6fc040417e0d61cee3d
Chris <gitlab@chriswilliams.penguincoder.com>
2020-12-22 13:49:34 -0600

    Add handling for starting local HTTP golang serve for development

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/zupzup/blog-generator/cli"
 )
 
 func main() {
-	cli.Run()
+	serveFlag := flag.Bool("s", false, "start a local HTTP server to view static files for local testing")
+
+	flag.Parse()
+
+	argNums := len(os.Args)
+
+	if argNums < 2 {
+		cli.Run()
+	} else {
+		if *serveFlag {
+			cli.Serve()
+		} else {
+			fmt.Println("Improper arguments")
+		}
+	}
 }
